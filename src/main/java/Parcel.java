f690bfacb43ec597e20c95229da4c1de85716dc3
Farnoosh Johnson and Ryan Carlile <farnoosh@>
2016-04-25 13:50:38 -0700

    add tests for volume

--- src/main/java/Parcel.java
+++ src/main/java/Parcel.java
@@ -4,19 +4,36 @@ public class Parcel {
   private int mWidth;
   private int mHeight;
   private int mWeight;
+  private int mVolume;
+  private int mMiles;
+  private double mShipping;
 
-  public Parcel(int length, int width, int height, int weight) {
+  public Parcel(int length, int width, int height, int weight, int miles) {
     mLength = length;
     mWidth = width;
     mHeight = height;
     mWeight = weight;
+    mMiles = miles;
+    mVolume = length * width * height;
   }
 
- //  public int getLength() {
- //    return mLength;
- //  }
- //  public int getWidth() {
- //    return mWidth;
- // }
-
+  public int getLength() {
+    return mLength;
+  }
+  public int getWidth() {
+    return mWidth;
+  }
+  public int getHeight() {
+   return mHeight;
+  }
+  public int getWeight() {
+   return mWeight;
+  }
+  public int getVolume() {
+   return mVolume;
+  }
+  public double getShipping() {
+    mShipping = (mVolume * 0.10 * mWeight * mMiles);
+    return mShipping;
+  }
 }
--- src/test/java/ParcelTest.java
+++ src/test/java/ParcelTest.java
@@ -6,7 +6,39 @@ public class ParcelTest {
 //the app gets the input and returns the String
   @Test
   public void Parcel_isDimensionAndWeight_true() {
-    Parcel testParcel = new Parcel(3,4,6,2);
+    Parcel testParcel = new Parcel(3,4,6,2,2);
     assertEquals(true, testParcel instanceof Parcel);
+  }
+  @Test
+  public void Parcel_isLength_3() {
+    Parcel testParcel = new Parcel(3,4,6,2,2);
+    assertEquals(3, testParcel.getLength());
+  }
+  @Test
+  public void Parcel_isWidth_4() {
+    Parcel testParcel = new Parcel(3,4,6,2,2);
+    assertEquals(4, testParcel.getWidth());
+  }
+  @Test
+  public void Parcel_isHeight_6() {
+    Parcel testParcel = new Parcel(3,4,6,2,2);
+    assertEquals(6, testParcel.getHeight());
+  }
+  @Test
+  public void Parcel_isWeight_2() {
+    Parcel testParcel = new Parcel(3,4,6,2,2);
+    assertEquals(2, testParcel.getWeight());
+  }
+
+  @Test
+  public void Parcel_isVolume_72() {
+    Parcel testParcel = new Parcel(3,4,6,2,2);
+    assertEquals(72, testParcel.getVolume());
+  }
+  @Test
+  public void Parcel_isShipping_expected() {
+    Parcel testParcel = new Parcel(3,4,6,2,2);
+    double expected = 28.8;
+    assertEquals(28, (int) testParcel.getShipping());
   }
 }
